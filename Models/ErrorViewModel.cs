0b8cc7223f6f997ae109504d06c13e7107339d39
unknown <ruizvalenciajhoansebastian@gmail.com>
2025-10-21 08:49:55 -0500

    proyecto rebase

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace proyecto_sena.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
